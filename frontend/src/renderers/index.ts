4932878dd694ecab4a17e8526fb29bd28f79d551
ginaxu1 <167130561+ginaxu1@users.noreply.github.com>
2026-02-24 06:12:08 -0800

    refactor: frontend to use  `@opennsw/ui@0.1.0` and `@opennsw/ui@0.1.0` (#170)
    * Updates to publish @lsf/ui custom renderers to NPM
    
    * Address PR Comments
    
    * More updates to publish @opennsw/ui@0.1.0 to npm
    
    * Separate portals/ui package into 2 packages: @opennsw/ui and @opennsw/jsonforms-renderers
    
    * Remove old portals/ui directory
    
    * Debug UI bugs
    
    * Address PR Comments
    
    * Add back portals/apps/oga-app/package.json
    
    * Remove unused pckgs from package.json

--- frontend/src/renderers/index.ts
+++
@@ -1,8 +0,0 @@
-import { vanillaRenderers } from '@jsonforms/vanilla-renderers';
-import { FileControl, FileControlTester } from '@lsf/ui';
-
-export { FileControl, FileControlTester };
-export const customRenderers = [
-    ...vanillaRenderers,
-    { tester: FileControlTester, renderer: FileControl },
-];
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import * as path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
@@ -9,9 +8,4 @@ export default defineConfig({
   server: {
     port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT, 10) : 5174,
   },
-  resolve: {
-    alias: {
-      '@lsf/ui': path.resolve(__dirname, '../../ui/src')
-    }
-  }
 })
